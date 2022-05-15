6703d6ef31a3cc608d65d9af3f66ebf980ba732e
hf-since2021 <hrk.fj.0911mon@gmail.com>
2022-05-16 01:07:40 +0900

    nameでログインする

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,9 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  # validatableにはemailのuniquenessのバリデーションが定義されているので除外
+  # 自前でvalidationをいれること！
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable#, :validatable
-         
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
+  # devise_for :users
+  devise_for :users, controllers: {
+    sessions: 'users/sessions'
+  }
+
+  # scope '/:year_term' do
+
+  # end
 end
