5c776b91247c39569454751dfdc028e5583eef48
hf-since2021 <hrk.fj.0911mon@gmail.com>
2022-05-19 00:12:06 +0900

    ユーザー新規作成機能追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,21 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_logined_term
 
   def index
-    # @logined_term = current_user.year_term.includes([:year,:term])
-    @logined_term = User.includes([:year,:term,:year_term]).find(current_user.id)
+    @users = User.all
   end
   
+  def new
+    @users = User.all
+    @user = User.new
+  end
+
+  def create
+    # binding.pry
+    # "user"=>{"name"=>"akatsuka", "password"=>"fujio1234"}, "manager"=>"on"
+  end
+
   def select
     # 年度・学期を渡す
     @year_terms = YearTerm.includes([:year,:term]).order("year_terms.code DESC")
@@ -16,5 +26,11 @@ class UsersController < ApplicationController
     # ログインする年度・学期を変更する
     current_user.update(year_term_id: params[:year_term_id])
     redirect_to root_url
+  end
+
+  private
+
+  def set_logined_term
+    @logined_term = User.includes([:year,:term,:year_term]).find(current_user.id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root to: "users#index"
+  resources :users, only: [:index, :new, :create]
   resources :year_terms, only: [:show]
   devise_for :users,
     only: [:sessions],
