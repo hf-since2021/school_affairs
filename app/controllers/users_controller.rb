b6796ffe9fdfb7265236c87e81476b29fcc1886c
hf-since2021 <hrk.fj.0911mon@gmail.com>
2022-05-19 18:16:55 +0900

    ユーザー登録編集機能実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,8 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
   before_action :set_logined_term
-  before_action :set_users_list, only: [:index, :new]
+  # before_action :set_user, only: [:edit]
+  before_action :set_users_list, only: [:index, :new, :edit]
 
   def index
   end
@@ -15,8 +16,35 @@ class UsersController < ApplicationController
     user.add_role "admin" if params[:admin] == "on"
     user.add_role "manager" if params[:manager] == "on"
     user.add_role "read_only" if params[:read_only] == "on"
-    redirect_to action: :new
+    redirect_to edit_user_path(user)
     # redirect_to action: :index
+  end
+
+  def edit
+    @user = User.find(params[:id])
+    @roles = {admin: "", manager: "", read_only: ""}
+    @roles.keys.each{|k| @roles[k]="checked" if @user.has_role?(k)}
+    # @user.has_role?(:admin) ? @role[:admin]="checked" : @role[:admin]=""
+    # @user.has_role?(:manager) ? @role[:manager]="checked" : @role[:manager]=""
+    # @user.has_role?(:read_only) ? @role[:read_only]="checked" : @role[:read_only]=""
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @roles = {admin: "", manager: "", read_only: ""}
+    @roles.keys.each do |k|
+      if params[k].nil?
+        @user.remove_role k
+      else
+        @user.add_role k
+        @roles[k] = "checked"
+      end
+    end
+    unless params[:user][:password].empty?
+      @user.password = params[:user][:password]
+      @user.save
+    end
+    render :edit
   end
 
   def select
@@ -36,6 +64,10 @@ class UsersController < ApplicationController
   def set_logined_term
     @logined_term = User.includes([:year,:term,:year_term]).find(current_user.id)
   end
+
+  # def set_user
+  #   @user = User.find(params[:user])
+  # end
 
   def set_users_list
     @users = User.all.order("users.name ASC")
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,15 @@
 Rails.application.routes.draw do
   root to: "users#index"
-  resources :users, only: [:index, :new, :create]
-  resources :year_terms, only: [:show]
+
+  # ユーザー管理系
+  resources :users, only: [:index, :new, :create, :edit, :update]
+    get   'users/select', to: 'users#select', as: 'user_select'
+    patch 'users/switch', to: 'users#switch', as: 'user_switch'
   devise_for :users,
     only: [:sessions],
     controllers: {
       sessions: 'users/sessions'
     }
-  resources :users, only: [:index]
-    get   'users/select', to: 'users#select', as: 'user_select'
-    patch 'users/switch', to: 'users#switch', as: 'user_switch'
+  
+  resources :year_terms, only: [:show]
 end
