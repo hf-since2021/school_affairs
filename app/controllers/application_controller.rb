bcacd6ca20e9d474a04eda5c48ba2be31e150f26
hf-since2021 <hrk.fj.0911mon@gmail.com>
2022-05-20 16:55:05 +0900

    権限機能の試験的実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,12 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth
+  # 要・管理者権限の操作orアクセスをチェック
+  before_action :check_admin_authorization
+  # 権限のない操作をrootへリダイレクト
+  rescue_from CanCan::AccessDenied do |_exception|
+    redirect_to root_path, alert: '画面を閲覧する権限がありません。'
+  end
+
 
   private
 
@@ -7,6 +14,12 @@ class ApplicationController < ActionController::Base
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
       username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
+    end
+  end
+
+  def check_admin_authorization
+    if request.path.start_with?('/users/select')
+      authorize! :access_admin_page, :all
     end
   end
 end
