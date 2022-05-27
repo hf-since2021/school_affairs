16e50ef47c00eed9c0830f38718c45e9b1267f22
hf-since2021 <hrk.fj.0911mon@gmail.com>
2022-05-27 20:57:24 +0900

    教員所属管理（年度別教員情報管理）新規登録・編集機能実装

--- app/controllers/annual_teachers_controller.rb
+++ app/controllers/annual_teachers_controller.rb
@@ -15,28 +15,46 @@ class AnnualTeachersController < ApplicationController
   end
 
   def create
+    @teachers = Teacher.includes(:annual_teachers)
+    @teacher = Teacher.find(params[:teacher_id])
+    @jobs = Job.all
+    @sections = Section.all
+    @responsibilities = Responsibility.all
+
+    @annual_teacher = AnnualTeacher.create(annual_teacher_params)
+    # redirect_to action: :index
+    redirect_to action: :edit
   end
 
   def edit
     @teachers = Teacher.includes(:annual_teachers)
     @teacher = Teacher.find(params[:teacher_id])
-    @annual_teacher = AnnualTeacher.find_by(year_id: @logined_term.year.id,teacher_id: params[:teacher_id])
+    @annual_teacher = AnnualTeacher.find_by(year_id: @logined_term.year.id, teacher_id: params[:teacher_id])
     @jobs = Job.all
     @sections = Section.all
     @responsibilities = Responsibility.all
   end
 
   def update
+    @teachers = Teacher.includes(:annual_teachers)
+    @teacher = Teacher.find(params[:teacher_id])
+    @jobs = Job.all
+    @sections = Section.all
+    @responsibilities = Responsibility.all
+
+    @annual_teacher = AnnualTeacher.find_by(year_id: @logined_term.year.id, teacher_id: params[:teacher_id])
+    @annual_teacher.update(annual_teacher_params)
+    # redirect_to action: :index
+    redirect_to action: :edit
   end
-  
+
   private
 
   def set_logined_term
     @logined_term = User.includes([:year,:term,:year_term]).find(current_user.id)
   end
 
-  # def teacher_params
-  #   params.require(:teacher).permit(:code, :last_name, :first_name, :last_name_reading, 
-  #                                   :first_name_reading, :subject_area_id, :user_id)
-  # end
+  def annual_teacher_params
+    params.require(:annual_teacher).permit(:job_id, :section_id, :responsibility_id, :school_grade, :klass).merge(year_id: @logined_term.year.id, teacher_id: params[:teacher_id])
+  end
 end
