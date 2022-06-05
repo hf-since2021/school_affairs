f0586ca6448ddbe85ebe3ac60460c7373c5eafe4
hf-since2021 <hrk.fj.0911mon@gmail.com>
2022-06-05 09:29:51 +0900

    管理者権限の機能実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,11 @@ class ApplicationController < ActionController::Base
   before_action :check_admin_authorization
   # 権限のない操作をrootへリダイレクト
   rescue_from CanCan::AccessDenied do |_exception|
-    redirect_to root_path, alert: '画面を閲覧する権限がありません。'
+    # if user_signed_in?
+      redirect_to root_path, alert: '画面を閲覧する権限がありません。'
+    # else
+      # redirect_to new_user_session_path
+    # end
   end
 
 
@@ -21,7 +25,7 @@ class ApplicationController < ActionController::Base
   end
 
   def check_admin_authorization
-    if request.path.start_with?('/users/select')
+    if request.path.start_with?('/admin') # request.path.start_with?('/users/select')
       authorize! :access_admin_page, :all
     end
   end
--- config/routes.rb
+++ config/routes.rb
@@ -10,25 +10,20 @@ Rails.application.routes.draw do
   # resources :students, only: [:index, :new, :create]
   resources :annual_students, only: [:index, :new, :create]
 
-
-  # ユーザー管理系
-  resources :users, only: [:index, :new, :create, :edit, :update]
-    get  'users/select', to: 'users#select', as: 'user_select'
-    post 'users/switch', to: 'users#switch', as: 'user_switch'
-  devise_for :users,
-    only: [:sessions],
-    controllers: {
-      sessions: 'users/sessions'
-    }
-  resources :teachers,          only: [:index, :new, :create] do
-    resources :annual_teachers,   only: [:new, :create]
-    get   '/annual_teachers/edit',   to: 'annual_teachers#edit',   as: 'edit_annual_teacher'
-    patch '/annual_teachers/update', to: 'annual_teachers#update', as: 'annual_teacher'
-  end
-  resources :annual_teachers,   only: [:index]
-
-  # マスタメンテ系
-  # scope :admin do
+  # 管理者系
+  scope :admin do
+    # ユーザー管理系
+    resources :users, only: [:index, :new, :create, :edit, :update]
+      get  'users/select', to: 'users#select', as: 'user_select'
+      post 'users/switch', to: 'users#switch', as: 'user_switch'
+    # 教員管理
+    resources :teachers,          only: [:index, :new, :create] do
+      resources :annual_teachers,   only: [:new, :create]
+      get   '/annual_teachers/edit',   to: 'annual_teachers#edit',   as: 'edit_annual_teacher'
+      patch '/annual_teachers/update', to: 'annual_teachers#update', as: 'annual_teacher'
+    end
+    resources :annual_teachers,   only: [:index]
+    # マスタメンテ系
     resources :years,             only: [:index, :new, :create]
     resources :terms,             only: [:index, :new, :create]
     resources :year_terms,        only: [:index, :new, :create]
@@ -40,8 +35,15 @@ Rails.application.routes.draw do
     resources :jobs,              only: [:index, :new, :create]
     resources :sections,          only: [:index, :new, :create]
     resources :responsibilities,  only: [:index, :new, :create]
-  # end
+  end
 
+  # ログイン
+  devise_for :users,
+  only: [:sessions],
+  controllers: {
+    sessions: 'users/sessions'
+  }
+  
   # 欠席連絡・出欠席管理
   resources :contacts, only: [:index]
 end
